c9f1ecfa7c1515a20d5ac12415fcef7927517db1
lucasbrt <lucasbritodepaula.t@gmail.com>
2025-11-15 08:43:14 -0300

    feat: detecta automaticamente as rotas para a ApiDoc

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,38 @@
+use axum::{Router, extract::Path, http::StatusCode, serve};
 use std::net::Ipv4Addr;
-
-use axum::{
-    Router,
-    extract::Path,
-    http::StatusCode,
-    routing::{get, post},
-    serve,
-};
 use tokio::net::TcpListener;
+use utoipa::OpenApi;
+use utoipa_axum::{router::OpenApiRouter, routes};
 
 const PORT: u16 = 3000;
 
+#[derive(OpenApi)]
+#[openapi(info(
+    description = "uma aplicação simples para testar as capacidades de detecção automática de rotas do Utoipa com Axum"
+))]
+struct ApiDoc;
+
+#[utoipa::path(get, path = "/hello")]
 async fn hello() -> (StatusCode, String) {
     (StatusCode::OK, "Hello, world!".to_string())
 }
 
+#[utoipa::path(
+    get,
+    path = "/say/{text}",
+    params(
+        ("text" = String, Path, description = "O texto a ser repetido pelo servidor")
+    ),
+)]
 async fn say(Path(text): Path<String>) -> (StatusCode, String) {
     println!("{text}");
     (StatusCode::OK, text)
 }
 
-fn main_router() -> Router {
-    Router::new()
-        .route("/hello", get(hello))
-        .route("/say/{text}", post(say))
+fn main_router() -> OpenApiRouter {
+    OpenApiRouter::new()
+        .routes(routes!(say))
+        .routes(routes!(hello))
 }
 
 async fn setup_tcp_listener() -> TcpListener {
@@ -38,8 +47,13 @@ async fn launch_server(listener: TcpListener, router: Router) {
 
 #[tokio::main]
 async fn main() {
-    let router = main_router();
     let listener = setup_tcp_listener().await;
+
+    let openapi_router = OpenApiRouter::with_openapi(ApiDoc::openapi()).merge(main_router());
+
+    let (router, doc) = openapi_router.split_for_parts();
+
+    println!("api docs: {:#?}", doc.paths.paths.keys());
 
     println!(
         "server launched at http://{}:{}/",
